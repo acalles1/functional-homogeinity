b5c7a49e10a6afd2eb95c4ac0791e41f83a21874
Alejandro Calle Saldarriaga <Alejandro.Calle434@gmail.com>
2019-09-14 10:22:30 -0500

    Add files via upload

--- R/TESTER.R
+++ R/TESTER.R
@@ -15,348 +15,67 @@ library(car)
 library(pracma)
 library(ddalpha)
 
-#source("generate_curves.R")
-
-Bootstrapper <- function(J, G, B=1000, depth.function=depthf.fd1){
-  # Computes the bootstrap statistics parallely.
-  #
-  # Args:
-  # J: First sample, normally the reference sample: Sample 0.
-  # G: Second sample, noramlly one of the other samples: Sample 1 to 5.
-  # B: The amount of bootstrap statistics we're going to use.
-  # depth.function: Pick any functional depth you prefer. The default depth
-  # is the Fraiman-Muniz depth, defined in Fraiman and Muniz [2001].
-  # Note: depth.RT does not work. It might be some error in implementation.
-  # nc: Number of clusters. In my PC I have 4 so this is the default value I
-  # will use.
-  #
-  # Returns:
-  # J.fun and G.fun: A list containing this two structures, which are
-  # the resampled versions of J and G.
-  #
-  # Note: Not currently using beta0 for anything, but some versions of this code
-  # used it. If you want to experiment with beta1 and beta0 simulataneously
-  # its quite easy as the t-values for beta0 are computed here too.
-  #
-  #cl <- makeCluster(nc)
-  Hh <- rbind(J, G)
-  Hf <- rawfd2dataf(Hh, c(0,1))
-  Jf <- rawfd2dataf(J, c(0,1))
-  Gf <- rawfd2dataf(G, c(0,1))
-  depths.inJ <- depth.function(Hf, Jf, range=c(0,1), d=30)
-  depths.inG <- depth.function(Hf, Gf, range=c(0,1), d=30)
-  depy <- depths.inJ$Half_FD
-  depx <- depths.inG$Half_FD
-  lm.ori <- lm(depy ~ depx)
-  b1.ori <- lm.ori$coefficients[2]
-  b0.ori <- lm.ori$coefficients[1]
-  beta0 <- numeric(B)
-  beta1 <- numeric(B)
-  t0 <- numeric(B)
-  t1 <- numeric(B)
-  kH <- dim(Hh)[1]
-  kN <- dim(J)[1]
-  kM <- dim(G)[1]
-  for (i in 1:B){
-    #print(i)
-    #Resample and compute the statistic.
-    Hs <- Hh[sample(1:kH,size=kH,replace=TRUE),]
-    Hsf <- rawfd2dataf(Hs, c(0,1))
-    Js <- Hs[1:kN, ]
-    aux <- kN + 1
-    #Next kH functions in H are the functions in the resampled version of H.
-    Gs <- Hs[aux:kH, ]
-    Jfs <- rawfd2dataf(Js, c(0,1))
-    Gfs <- rawfd2dataf(Gs, c(0,1))
-    #Resampled depth, s
-    depths.inJs <- depth.function(Hsf, Jfs, range=c(0,1), d=30)
-    depths.inGs <- depth.function(Hsf, Gfs, range=c(0,1), d=30)
-    depys <- depths.inJs$Half_FD
-    depxs <- depths.inGs$Half_FD
-    lm.bs <- lm(depys ~ depxs)
-    # Betas
-    beta1[i] <- lm.bs$coefficients[2]
-    beta0[i] <- lm.bs$coefficients[1]
-    sum_aux <- sum((depxs - mean(depxs))^2)
-    # Formula for the standard deviation of both betas. Standard formula from
-    # regression analysis.
-    beta1.std <- sqrt((sum(lm.bs$residuals^2))/((kH-2)*sum_aux))
-    beta0.std <- sqrt((sum(lm.bs$residuals^2)*sum(depxs^2))/((kH-2)*sum_aux))
-    t1[i] <- (beta1[i] - 1)/beta1.std
-    t0[i] <- (beta0[i])/beta0.std
-  }
-  stats <- list()
-  stats$b0 <- beta0
-  stats$b1 <- beta1
-  stats$t0 <- t0
-  stats$t1 <- t1
-  return(stats)
-}
-
-Tester <- function(J, G, B=1000, depth.function=depthf.fd1){
-  # Tests homogeinity using bootstrap confidence intervals. H0: Homogeinity.
-  #
-  # Args:
-  # J: First sample, normally the reference sample: Sample 0.
-  # G: Second sample, noramlly one of the other samples: Sample 1 to 5.
-  # B: The amount of bootstrap statistics we're going to use.
-  # stat: The statistic we are going to use. You can choose P1-P4.
-  # depth.function: Pick any functional depth you prefer. The default depth
-  # is the Fraiman-Muniz depth, defined in Fraiman and Muniz [2001].
-  # Note: depth.RT does not work. It might be some error in implementation.
-  # nc: Number of clusters. In my PC I have 4 so this is the default value I
-  # will use.
-  #
-  # Returns:
-  # TRUE if we do not reject the H0, meaning that J and G come from the same
-  # population. False is we reject the H0, meaning that J and G are not
-  # homogeneous and come from different popula 
-  Hh <- rbind(J, G)
-  kH <- dim(Hh)[1]
-  Hf <- rawfd2dataf(Hh, c(0,1))
-  Jf <- rawfd2dataf(J, c(0,1))
-  Gf <- rawfd2dataf(G, c(0,1))
-  depths.inJ <- depth.function(Hf, Jf, range=c(0,1), d=30)
-  depths.inG <- depth.function(Hf, Gf, range=c(0,1), d=30)
-  depy <- depths.inJ$Half_FD
-  depx <- depths.inG$Half_FD
-  lm.ori <- lm(depy ~ depx)
-  b1.ori <- lm.ori$coefficients[2]
-  b0.ori <- lm.ori$coefficients[1]
-  stats <- Bootstrapper(J, G, B=B, depth.function=depth.function)
-  # Extract all statistics from the bootstrapper.
-  b0 <- stats$b0
-  b1 <- stats$b1
-  t0 <- stats$t0
-  t1 <- stats$t1
-  sum_aux <- sum((depx - mean(depx))^2)
-  beta1.std <- sqrt((sum(lm.ori$residuals^2))/((kH-2)*sum_aux))
-  T.Test1 <- (b1.ori - 1)/beta1.std
-  dist.t1 <- ecdf(t1)
-  neg.T1 <- min(T.Test1, -T.Test1)
-  p1.half.neg <- dist.t1(neg.T1)
-  p1.half.pos <- 1.0 - dist.t1(-neg.T1)
-  p1 <- p1.half.neg + p1.half.neg
-  beta0.std <- sqrt((sum(lm.ori$residuals^2)*sum(depx^2))/((kH-2)*sum_aux))
-  T.Test0 <- b0.ori/beta0.std
-  dist.t0 <- ecdf(t0)
-  neg.T0 <- min(T.Test0, -T.Test0)
-  p0.half.neg <- dist.t0(neg.T0)
-  p0.half.pos <- 1.0 - dist.t0(-neg.T0)
-  p0 <- p0.half.neg + p0.half.pos
-  p <- min(p0, p1)
-  # 5% cutoff (2.5% upper and lower).
-  t0crit <- quantile(t0, probs=c(0.025, 0.975))
-  t1crit <- quantile(t1, probs=c(0.025, 0.975))
-  beta0.l <- b0.ori - t0crit[2]*beta0.std
-  beta0.u <- b0.ori - t0crit[1]*beta0.std
-  beta1.l <- b1.ori - t1crit[2]*beta1.std
-  beta1.u <- b1.ori - t1crit[1]*beta1.std
-  res <- t0crit[1] <= T.Test0 && t0crit[2] >= T.Test0 && t1crit[1] <= T.Test1 && t1crit[2] >= T.Test1
-  returns <- list()
-  returns$res <- res
-  returns$p <- p
-  returns$t0crit <- t0crit
-  returns$t1crit <- t1crit
-  returns$T1 <- T.Test1
-  returns$T0 <- T.Test0
-  return(returns)
-}
-
-Bootstrapper2 <- function(J, G, B=1000, depth.function=depthf.fd1){
-  # Computes the bootstrap statistics parallely.
-  #
-  # Args:
-  # J: First sample, normally the reference sample: Sample 0.
-  # G: Second sample, noramlly one of the other samples: Sample 1 to 5.
-  # B: The amount of bootstrap statistics we're going to use.
-  # depth.function: Pick any functional depth you prefer. The default depth
-  # is the Fraiman-Muniz depth, defined in Fraiman and Muniz [2001].
-  # Note: depth.RT does not work. It might be some error in implementation.
-  # nc: Number of clusters. In my PC I have 4 so this is the default value I
-  # will use.
-  #
-  # Returns:
-  # J.fun and G.fun: A list containing this two structures, which are
-  # the resampled versions of J and G.
-  #
-  # Note: Not currently using beta0 for anything, but some versions of this code
-  # used it. If you want to experiment with beta1 and beta0 simulataneously
-  # its quite easy as the t-values for beta0 are computed here too.
-  #
-  #cl <- makeCluster(nc)
-  Hh <- rbind(J, G)
-  Hf <- rawfd2dataf(Hh, c(0,1))
-  Jf <- rawfd2dataf(J, c(0,1))
-  Gf <- rawfd2dataf(G, c(0,1))
-  depths.inJ <- depth.function(Hf, Jf, range=c(0,1), d=30, order=2, approx=150)
-  depths.inG <- depth.function(Hf, Gf, range=c(0,1), d=30, order=2, approx=150)
-  depy <- depths.inJ$Half_FD
-  depx <- depths.inG$Half_FD
-  lm.ori <- lm(depy ~ depx)
-  b1.ori <- lm.ori$coefficients[2]
-  b0.ori <- lm.ori$coefficients[1]
-  beta0 <- numeric(B)
-  beta1 <- numeric(B)
-  t0 <- numeric(B)
-  t1 <- numeric(B)
-  kH <- dim(Hh)[1]
-  kN <- dim(J)[1]
-  kM <- dim(G)[1]
-  for (i in 1:B){
-    #print(i)
-    #Resample and compute the statistic.
-    Hs <- Hh[sample(1:kH,size=kH,replace=TRUE),]
-    Hsf <- rawfd2dataf(Hs, c(0,1))
-    Js <- Hs[1:kN, ]
-    aux <- kN + 1
-    #Next kH functions in H are the functions in the resampled version of H.
-    Gs <- Hs[aux:kH, ]
-    Jfs <- rawfd2dataf(Js, c(0,1))
-    Gfs <- rawfd2dataf(Gs, c(0,1))
-    #Resampled depth, s
-    depths.inJs <- depth.function(Hsf, Jfs, range=c(0,1), order=2, approx=150)
-    depths.inGs <- depth.function(Hsf, Gfs, range=c(0,1), order=2, approx=150)
-    depys <- depths.inJs$Half_FD
-    depxs <- depths.inGs$Half_FD
-    lm.bs <- lm(depys ~ depxs)
-    # Betas
-    beta1[i] <- lm.bs$coefficients[2]
-    beta0[i] <- lm.bs$coefficients[1]
-    sum_aux <- sum((depxs - mean(depxs))^2)
-    # Formula for the standard deviation of both betas. Standard formula from
-    # regression analysis.
-    beta1.std <- sqrt((sum(lm.bs$residuals^2))/((kH-2)*sum_aux))
-    beta0.std <- sqrt((sum(lm.bs$residuals^2)*sum(depxs^2))/((kH-2)*sum_aux))
-    t1[i] <- (beta1[i] - 1)/beta1.std
-    t0[i] <- (beta0[i])/beta0.std
-  }
-  stats <- list()
-  stats$b0 <- beta0
-  stats$b1 <- beta1
-  stats$t0 <- t0
-  stats$t1 <- t1
-  return(stats)
-}
-
-Tester2 <- function(J, G, B=1000, depth.function=depthf.fd1){
-  # Tests homogeinity using bootstrap confidence intervals. H0: Homogeinity.
-  #
-  # Args:
-  # J: First sample, normally the reference sample: Sample 0.
-  # G: Second sample, noramlly one of the other samples: Sample 1 to 5.
-  # B: The amount of bootstrap statistics we're going to use.
-  # stat: The statistic we are going to use. You can choose P1-P4.
-  # depth.function: Pick any functional depth you prefer. The default depth
-  # is the Fraiman-Muniz depth, defined in Fraiman and Muniz [2001].
-  # Note: depth.RT does not work. It might be some error in implementation.
-  # nc: Number of clusters. In my PC I have 4 so this is the default value I
-  # will use.
-  #
-  # Returns:
-  # TRUE if we do not reject the H0, meaning that J and G come from the same
-  # population. False is we reject the H0, meaning that J and G are not
-  # homogeneous and come from different popula 
-  Hh <- rbind(J, G)
-  kH <- dim(Hh)[1]
-  Hf <- rawfd2dataf(Hh, c(0,1))
-  Jf <- rawfd2dataf(J, c(0,1))
-  Gf <- rawfd2dataf(G, c(0,1))
-  depths.inJ <- depth.function(Hf, Jf, range=c(0,1), order=2, approx=150)
-  depths.inG <- depth.function(Hf, Gf, range=c(0,1), order=2, approx=150)
-  depy <- depths.inJ$Half_FD
-  depx <- depths.inG$Half_FD
-  lm.ori <- lm(depy ~ depx)
-  b1.ori <- lm.ori$coefficients[2]
-  b0.ori <- lm.ori$coefficients[1]
-  stats <- Bootstrapper2(J, G, B=B, depth.function=depth.function)
-  # Extract all statistics from the bootstrapper.
-  b0 <- stats$b0
-  b1 <- stats$b1
-  t0 <- stats$t0
-  t1 <- stats$t1
-  sum_aux <- sum((depx - mean(depx))^2)
-  beta1.std <- sqrt((sum(lm.ori$residuals^2))/((kH-2)*sum_aux))
-  T.Test1 <- (b1.ori - 1)/beta1.std
-  dist.t1 <- ecdf(t1)
-  neg.T1 <- min(T.Test1, -T.Test1)
-  p1.half.neg <- dist.t1(neg.T1)
-  p1.half.pos <- 1.0 - dist.t1(-neg.T1)
-  p1 <- p1.half.neg + p1.half.neg
-  beta0.std <- sqrt((sum(lm.ori$residuals^2)*sum(depx^2))/((kH-2)*sum_aux))
-  T.Test0 <- b0.ori/beta0.std
-  dist.t0 <- ecdf(t0)
-  neg.T0 <- min(T.Test0, -T.Test0)
-  p0.half.neg <- dist.t0(neg.T0)
-  p0.half.pos <- 1.0 - dist.t0(-neg.T0)
-  p0 <- p0.half.neg + p0.half.pos
-  p <- min(p0, p1)
-  # 5% cutoff (2.5% upper and lower).
-  t0crit <- quantile(t0, probs=c(0.025, 0.975))
-  t1crit <- quantile(t1, probs=c(0.025, 0.975))
-  beta0.l <- b0.ori - t0crit[2]*beta0.std
-  beta0.u <- b0.ori - t0crit[1]*beta0.std
-  beta1.l <- b1.ori - t1crit[2]*beta1.std
-  beta1.u <- b1.ori - t1crit[1]*beta1.std
-  res <- t0crit[1] <= T.Test0 && t0crit[2] >= T.Test0 && t1crit[1] <= T.Test1 && t1crit[2] >= T.Test1
-  returns <- list()
-  returns$res <- res
-  returns$p <- p
-  returns$t0crit <- t0crit
-  returns$t1crit <- t1crit
-  returns$T1 <- T.Test1
-  returns$T0 <- T.Test0
-  return(returns)
-}
 
 source("/home/cauchy/acalles/functional-homogeinity-master/R/generate_curves.R")
+source("/home/cauchy/acalles/functional-homogeinity-master/R/dd-otherdepths.R")
 K <- 100
-res_ord1 <- matrix(0, nrow = 6, ncol = 6)
-res_ord2 <- matrix(0, nrow = 6, ncol = 6)
 
-for (i in 1:6){
-   for (j in i:6){
-      print('J')
-      print(i)
-      print('G')
-      print(j)
-      trials <- seq(1:K)
-      fx1 <- function(trial){
-         S <- GenerateCurves()
-         J <- S[[i]]
-         S <- GenerateCurves()
-         G <- S[[j]]
-         resu <- Tester(J, G, B=1000)
-         res <- resu$res
-      }
-      fx2 <- function(trial){
-         S <- GenerateCurves()
-         J <- S[[i]]
-         S <- GenerateCurves()
-         G <- S[[j]]
-         resu <- Tester2(J, G, B=1000)
-         res <- resu$res
-      }
-      tests1 <- mclapply(trials, fx1, mc.cores=8)
-      tests2 <- mclapply(trials, fx2, mc.cores=8)
-      suma1 <- 0
-      suma2 <- 0
-      for (k in 1:K){
-        if (tests1[[k]]){
-           suma1 <- suma1 + 1
-        }
-        if (tests2[[i]]){
-           suma2 <- suma2 + 1
-        }
-      }
-      res_ord1[i, j] <- 1 - suma1/K
-      print('res1')
-      print(res_ord1[i, j])
-      print('res2')
-      res_ord2[i, j] <- 1 - suma2/K
-      print(res_ord2[i,j])
-   }
-}
+#res_mat <- matrix(0, nrow=6, ncol=6)
+#for (i in 1:6){
+#   for (j in i:6){
+#      print('model0')
+#      print(i)
+#      print('model1')
+#      print(j)
+#      trials <- seq(1:K)
+#      fx1 <- function(trial){
+#         S <- GenerateCurves()
+#         J <- S[[i]]
+#         S <- GenerateCurves()
+#         G <- S[[j]]
+#         resu <- Tester.od(J, G, B=1000)
+#      }
+#      tests1 <- mclapply(trials, fx1, mc.cores=8)
+#      suma1 <- 0
+#      for (k in 1:K){
+#        if (tests1[[k]]$res){
+#           suma1 <- suma1 + 1
+#        }
+#      }
+#      res_mat[i,j] <- 1 - suma1/K
+#      print(res_mat[i,j])
+#   }
+#}
 
-res_ord1
-res_ord2
+#res_mat
+
+#for (i in 1:n_deltas){
+#      print('delta')
+#      print(deltas[i])
+#      trials <- seq(1:K)
+#      fx1 <- function(trial){
+#         S0 <- Generator()
+#         J <- S0
+#         S1 <- Generator(delta=deltas[i])
+#         G <- S1
+#         resu <- Tester(J, G, B=1000)
+#         res <- resu
+#      }
+#      tests1 <- mclapply(trials, fx1, mc.cores=8)
+#      suma1 <- 0
+#     pnow <- 0
+#      for (k in 1:K){
+#        if (tests1[[k]]$res){
+#           suma1 <- suma1 + 1
+#        }
+#        pnow <- pnow + tests1[[k]]$p
+#      }
+#      print('res')
+#      results[i] <- 1 - suma1/K
+#      ps[i] <- pnow/K
+#      print(results[i])
+#      print('p-val')
+#      print(ps[i])
+#}
+
+#results
