fc17b72f0dbc7242b569e9f92782f87be3bb04f7
Alejandro Calle Saldarriaga <Alejandro.Calle434@gmail.com>
2019-10-17 08:45:03 -0500

    change parameters in mvn

--- R/dd_plot_multivariate.R
+++ R/dd_plot_multivariate.R
@@ -3,14 +3,16 @@ library(depth)
 library(ggplot2)
 library(MASS)
 library(tibble)
+library(ggthemes)
 
 theme_set(theme_tufte())
 kNc <- 500
 kNs <- 2
 cov.e <- matrix(c(1, 0, 0, 1), nrow=kNs, ncol=kNs)
+cov.h <- matrix(c(1.5, 0.3, 0.3, 1.5))
 # Initializ covariance matrix for h(t) with zeroes.
 mu1 <- numeric(kNs)
-mu2 <- c(0.5, 0)
+mu2 <- c(0.5, 1.3)
 s1 <- mvrnorm(n = kNc, mu=mu1, Sigma=cov.e)
 s2 <- mvrnorm(n = kNc, mu=mu2, Sigma=cov.e)
 s <- rbind(s1, s2)
